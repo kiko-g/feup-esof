ca26c047936e544a9a7f59edd1fd75ff3c152c07
Martim de Carvalho e Sousa Pinto da Silva <41308685+motapinto@users.noreply.github.com>
2019-11-07 15:48:01 +0000

    Profile widget 100% done

--- campus_nav/lib/view/Widgets/MyProfileWidget.dart
+++ campus_nav/lib/view/Widgets/MyProfileWidget.dart
@@ -1,40 +1,25 @@
-//Flutter widgets
+//IO operations and variables
 import 'dart:io';
-
+import 'dart:math';
+//Flutter widgets 
 import 'package:flutter/material.dart';
 //Image picker to select photo from camera
 import 'package:image_picker/image_picker.dart';
 
 
 class MyProfileWidget extends StatefulWidget {
-  final name, interests, gender;
-  
-  MyProfileWidget({
-    @required this.name, 
-    @required this.interests, 
-    @required this.gender
-  });
 
   @override
   _MyProfileWidgetState createState() => 
-  _MyProfileWidgetState(
-    name: name,
-    interests: interests,
-    gender: gender
-  );
+  _MyProfileWidgetState();
 }
 
 class _MyProfileWidgetState extends State<MyProfileWidget> {
-  var name, interests, gender;
   File img;
   final defaultProfile = "assets/images/profileDefault.png";
-  bool sports = false, science = false, tech = false, softw = false, bussiness = false;
-
-  _MyProfileWidgetState({
-    @required this.name, 
-    @required this.interests, 
-    @required this.gender
-  });
+  
+  var name="Martim", gender="Male", image="";
+  bool sports = false, science = false, tech = false, softw = false, business = false;
 
   @override
   void initState() {
@@ -49,10 +34,12 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
 
   updateImageGallery() async{
     img = await ImagePicker.pickImage(source: ImageSource.gallery);
+    image = img.path;
   }
 
   updateImageCamera() async{
     img = await ImagePicker.pickImage(source: ImageSource.camera);
+    image = img.path;
   }
 
   updateGender() {
@@ -66,70 +53,91 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
       sports == false ? sports = true : sports = false;
     });
   }
+
   updateScience() {
     setState(() {
       science == false ? science = true : science = false;
     });
   }
+
   updateTech() {
     setState(() {
       tech == false ? tech = true : tech = false;
     });
   }
+
   updateSoftw() {
     setState(() {
       softw == false ? softw = true : softw = false;
     });
   }
-  updateBussiness() {
+
+  updateBusiness() {
     setState(() {
-      bussiness == false ? bussiness = true : bussiness = false;
+      business == false ? business = true : business = false;
     });
   }
 
-  //PROBLEMA QUANDO MECHO NO TICK SPORTS AFETA BUSSINESS
-
   @override
   Widget build(BuildContext context){
-    return new Column(
-      children: <Widget>[
+    return new SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
         //Profile image and edit icon
-        Stack(
-          children: <Widget>[
-            Container(
-              width: 350,
-              height: 200,
-              child: Align(
-                alignment: Alignment.centerRight,
-                child: IconButton(
-                  onPressed: updatePhoto,
-                  icon: Icon(Icons.edit)
-                ), 
-              ),  
-            ),
-            Container(
-              width: 400,
-              height: 200,
-              child: Align(
-                alignment: Alignment.center,
-                child: ClipOval(
-                  child: Image.asset(img==null?defaultProfile:img.path,
-                    fit: BoxFit.fitWidth,
-                    width: 150.0,
-                    height: 150.0,
-                  )
+          Stack(
+            children: <Widget>[
+              //Camera icon
+              Container(
+                width: 350,
+                height: 150,
+                child: Align(
+                  alignment: Alignment.bottomRight,
+                  child: IconButton(
+                    onPressed: updatePhoto,
+                    icon: Icon(Icons.photo)
+                  ), 
+                ),  
+              ),
+              //Gallery icon
+              Container(
+                width: 350,
+                height: 150,
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    onPressed: updatePhoto,
+                    icon: Icon(Icons.photo_camera)
+                  ), 
+                ),  
+              ),
+              //Profile photon
+              Container(
+                width: 400,
+                height: 200,
+                child: Align(
+                  alignment: Alignment.center,
+                  child: ClipOval(
+                    child: Image.asset(image==""?defaultProfile:image,
+                      fit: BoxFit.fitWidth,
+                      width: 150.0,
+                      height: 150.0,
+                    )
+                  ),
                 ),
               ),
-            ),
-          ],
-        ),
-
-        Container(
-          width: 500,
-          height: 50,
-          child: Align(
-            alignment: Alignment.topCenter,
-            child: Text(name,
+            ],
+          ),
+          //Name
+          Container(
+            width: 300,
+            height: 100,
+            alignment: Alignment.center,
+            child: TextFormField(
+              initialValue: name,
+              onSaved: (text) {
+                name = text;
+              },
               style: TextStyle(
                 color: Colors.white,
                 fontSize: 30,
@@ -137,62 +145,62 @@ class _MyProfileWidgetState extends State<MyProfileWidget> {
               ),
             ),
           ),
-        ),
-        //Gender
-        ListTile(
-          trailing: IconButton(
-            tooltip: "Edit attribute",
-            icon: Icon(Icons.edit),
-            onPressed: updateGender,
+          //Interests and gender
+          ListTile(
+            trailing: IconButton(
+              tooltip: "Edit attribute",
+              icon: Icon(Icons.edit),
+              onPressed: updateGender,
+            ),
+            title: Text('Gender: ' + gender),
           ),
-          title: Text('Gender: ' + gender),
-        ),
-        ExpansionTile(
-          title: Text("My Interests"),
-          children: <Widget>[
-            ListTile(
-              trailing: IconButton(
-                tooltip: "Edit attribute",
-                icon: Icon(sports == true ? Icons.check_circle : Icons.check_circle_outline),
-                onPressed: updateSports,
+          ExpansionTile(
+            title: Text("My Interests"),
+            children: <Widget>[
+              ListTile(
+                trailing: IconButton(
+                  tooltip: "Edit attribute",
+                  icon: Icon(sports == true ? Icons.check_circle : Icons.check_circle_outline),
+                  onPressed: updateSports,
+                ),
+                title: Text('Sports', style: TextStyle(decoration: TextDecoration.underline),),
               ),
-              title: Text('Sports', style: TextStyle(decoration: TextDecoration.underline),),
-            ),
-            ListTile(
-              trailing: IconButton(
-                tooltip: "Edit attribute",
-                icon: Icon(science == true ? Icons.check_circle : Icons.check_circle_outline),
-                onPressed: updateScience,
+              ListTile(
+                trailing: IconButton(
+                  tooltip: "Edit attribute",
+                  icon: Icon(science == true ? Icons.check_circle : Icons.check_circle_outline),
+                  onPressed: updateScience,
+                ),
+                title: Text('Science', style: TextStyle(decoration: TextDecoration.underline),),
               ),
-              title: Text('Science', style: TextStyle(decoration: TextDecoration.underline),),
-            ),
-            ListTile(
-              trailing: IconButton(
-                tooltip: "Edit attribute",
-                icon: Icon(tech == true ? Icons.check_circle : Icons.check_circle_outline),
-                onPressed: updateTech,
+              ListTile(
+                trailing: IconButton(
+                  tooltip: "Edit attribute",
+                  icon: Icon(tech == true ? Icons.check_circle : Icons.check_circle_outline),
+                  onPressed: updateTech,
+                ),
+                title: Text('Tech', style: TextStyle(decoration: TextDecoration.underline),),
               ),
-              title: Text('Tech', style: TextStyle(decoration: TextDecoration.underline),),
-            ),
-            ListTile(
-              trailing: IconButton(
-                tooltip: "Edit attribute",
-                icon: Icon(softw == true ? Icons.check_circle : Icons.check_circle_outline),
-                onPressed: updateSoftw,
+              ListTile(
+                trailing: IconButton(
+                  tooltip: "Edit attribute",
+                  icon: Icon(softw == true ? Icons.check_circle : Icons.check_circle_outline),
+                  onPressed: updateSoftw,
+                ),
+                title: Text('Software Engineering', style: TextStyle(decoration: TextDecoration.underline),),
               ),
-              title: Text('Software Engineering', style: TextStyle(decoration: TextDecoration.underline),),
-            ),
-            ListTile(
-              trailing: IconButton(
-                tooltip: "Edit attribute",
-                icon: Icon(sports == bussiness ? Icons.check_circle : Icons.check_circle_outline),
-                onPressed: updateBussiness,
+              ListTile(
+                trailing: IconButton(
+                  tooltip: "Edit attribute",
+                  icon: Icon(business == true ? Icons.check_circle : Icons.check_circle_outline),
+                  onPressed: updateBusiness,
+                ),
+                title: Text('Business', style: TextStyle(decoration: TextDecoration.underline),),
               ),
-              title: Text('Business', style: TextStyle(decoration: TextDecoration.underline),),
-            ),
-          ],
-        )
-      ],
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
